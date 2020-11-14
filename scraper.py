bec3e668a13a8b4e49ccc9a9b6687a44664cad94
Manuel <pfandlmanuel@protonmail.com>
2020-11-14 18:17:31 +0100

    organize codebase

--- scraper.py
+++ scraper.py
@@ -3,26 +3,35 @@ from bs4 import BeautifulSoup as bs4
 from selenium import webdriver
 
 
-def get_url(search_term):
+def get_url(search_term, page):
     #generate URL from search_term
-    template = "https://www.amazon.de/s?k={}"
+    template = "https://www.amazon.de/s?k={}&page=" + str(page)
     search_term = search_term.replace(" ", "+")
     return template.format(search_term)
 
 
-if __name__ == "__main__":
+def get_soup():
     #startup the webdriver
     driver = webdriver.Chrome()
 
     #include keyword to url
-    url = get_url("organizer")
+    page = 2    #page of search
+    url = get_url("organizer", page)
+
+    #load webpage
     driver.get(url)
-    print(url)
 
     #get page soup
     soup = bs4(driver.page_source, "html.parser")
 
-    #get item
+    #close webdriver
+    driver.close()
+
+    return soup
+
+
+def get_items(soup):
+    #get all individual items from the webpage
     results = soup.find_all("div", {"data-component-type": "s-search-result"})
 
     for idx, item in enumerate(results):
@@ -42,9 +51,7 @@ if __name__ == "__main__":
         print("Is Product Organic: " + str(not_sponsored))
         print("########")
 
-    #close webdriver
-    driver.close()
 
-'''
-class="aok-inline-block s-sponsored-label-info-icon" - Sponsored product Tag
-'''
+if __name__ == "__main__":
+    soup = get_soup()
+    get_items(soup)
